b9f2fcafa4e421a644160473e4f05126808ed325
Aaditya Jha <38150984+aadi2057@users.noreply.github.com>
2019-02-15 18:36:17 +0545

    All files of django project is added

---
+++ apps/user_profile/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Profile(models.Model):
+    contact_no = models.CharField(max_length=15)
+    address = models.CharField(max_length=50)
+    nationality = models.CharField(max_length=20)
+    gender = models.CharField(max_length=6)
+    dob = models.DateField()
+    occupation = models.CharField(max_length=20)
+
+    def __str__(self):
+        return 'Contact No: {} , Date of Birth: {}'.format(self.contact_no,
+                                                        self.dob)
